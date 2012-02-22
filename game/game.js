57defd978e2d3f1e55958819fd9c61348f726190
jare <jare@iguanademos.com>
2012-02-22 02:03:55 +0100

    Error checking for Database.

--- game/game.js
+++ game/game.js
@@ -15,13 +15,14 @@ var util = require('util');
 
 var Rng = require('../lib/rng');
 
-var db = null;
+var database = null;
 
 var nextSessionId = 1;
 
 var InitDb = exports.InitDb = function(cb) {
-	if (db) {
-		if (cb) cb(null, db);
+	if (database) {
+		console.log("DB already set up, reusing it.")
+		if (cb) cb(null, database);
 		return;
 	}
 
@@ -29,37 +30,56 @@ var InitDb = exports.InitDb = function(cb) {
 	var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
 	console.log("Connecting to " + host + ":" + port);
-	db = new Db('Econ_fb', new Server(host, port, {}), {});
-	db.open(function(err, db) {
-		if (err) db = null;
+	database = new Db('Econ_fb', new Server(host, port, {}), {});
+	database.open(function(err, db) {
+		// If database is not available, clear global database object to avoid reuse
+		if (err) database = null;
 		if (cb) cb(err, db);
 	});
 }
 
 var NewSession = exports.NewSession = function(SNUserId, cb) {
 	InitDb(function(err, db) {
+		var newUser = {
+    		sn_id:SNUserId,
+    		num_sessions:0,
+    		seed: Rng.LCG.newSeed()
+    	};
+		if (err) {
+			// Fake session without DB access. Not that it matters anyway
+			console.log("No MongoDB available, playing without persistent sessions.\n" + err);
+			cb(newUser);
+			return;
+		}
 	    db.collection('users', function(err, collection) {
+			if (err) {
+				// Fake session without DB access. Not that it matters anyway
+				console.log("Eror opening collection, playing without persistent sessions.\n" + err);
+				cb(newUser);
+				return;
+			}
 	        collection.findOne({sn_id:SNUserId}, function(err, user) {
-	        	var onUser = function(user) {
+	        	var onUser = function(user, dbcollection) {
 	        		console.log("Found user " + util.inspect(user));
 	        		user.num_sessions++;
 	        		user.session_id = nextSessionId++;
-	        		collection.update({_id:user._id}, user);
+	        		dbcollection.update({_id:user._id}, user);
+	        		user.collection = dbcollection;
 	        		cb(user);
 	        	};
 
 	        	if (err || !user) {
-	        		var newUser = {
-		        		sn_id:SNUserId,
-		        		num_sessions:0,
-		        		seed: Rng.LCG.newSeed()
-		        	};
 	        		collection.insert(newUser, function(err, n) {
-	        			if (!err)
-		        			onUser(newUser);
+						if (err) {
+							// Fake session without DB access. Not that it matters anyway
+							console.log("Eror inserting new user, playing without persistent sessions.\n" + err);
+							cb(newUser);
+							return;
+						}
+	        			onUser(newUser, collection);
 	        		});
 	        	} else {
-	        		onUser(user);
+	        		onUser(user, collection);
 	        	}
 	        });          
 	    });
